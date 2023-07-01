69f13bfb794c25bf8ea7d9bae4548abe5564368c
joeygitari <gitarijoanne@gmail.com>
2023-07-01 08:54:55 +0000

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script send a request to a URL and displays the size of the body of the response
-curl -sI "$1" | grep  'Content-Length' | cut -d ' ' -f 2
-
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
