af02682cd13b56e6f3aa86e0e9e7256740c5c379
Joanne Gitari <75974016+joeygitari@users.noreply.github.com>
2023-07-26 13:41:00 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
+
+// Scripts that prints all characters
+// of a Star Wars movie
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) console.error(error);
+  if (response && response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
     characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+      request(character, (err, resp, bd) => {
+        if (err) console.error(err);
+        if (resp && resp.statusCode === 200) {
+          console.log(JSON.parse(bd).name);
         }
       });
     });
